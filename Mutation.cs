efc071738fdc335b3d33b04a4664ea79f75c2152
illunix <cros448@gmail.com>
2020-01-10 16:47:57 +0100

    init

---
+++ Mutation.cs
@@ -0,0 +1,21 @@
+﻿using Posts.API.Models;
+using Posts.API.Repositories;
+using System.Threading.Tasks;
+
+namespace Posts.API.Mutation
+{
+    public class Mutation
+    {
+        private readonly IPostsRepository _repository;
+
+        public Mutation(IPostsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task CreatePost(Post model)
+        {
+            await _repository.Add(model);
+        }
+    }
+}
---
+++ PostInputType.cs
@@ -0,0 +1,26 @@
+﻿using HotChocolate.Types;
+using Posts.API.Models;
+
+namespace Posts.API.Types
+{
+    public class PostInputType : InputObjectType<Post>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<Post> descriptor)
+        {
+            descriptor.Field(p => p.Id)
+                .Type<StringType>();
+
+            descriptor.Field(p => p.UserId)
+                .Type<StringType>();
+
+            descriptor.Field(p => p.Title)
+                .Type<StringType>();
+
+            descriptor.Field(p => p.Description)
+                .Type<StringType>();
+
+            descriptor.Field(p => p.Status)
+                .Type<StringType>();
+        }
+    }
+}
